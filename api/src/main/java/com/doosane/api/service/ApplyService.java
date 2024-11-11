88bf84f43c0b11efe636ef84b1dd14ff37ed5b88
baekdoosan <doosan0425@gmail.com>
2024-11-12 04:45:41 +0900

    1인당 쿠폰의 개수를 한개로 제한해봄

--- api/src/main/java/com/doosane/api/service/ApplyService.java
+++ api/src/main/java/com/doosane/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.doosane.api.service;
 
 import com.doosane.api.domain.Coupon;
 import com.doosane.api.producer.CouponCreateProducer;
+import com.doosane.api.repository.AppliedUserRepository;
 import com.doosane.api.repository.CouponCountRepository;
 import com.doosane.api.repository.CouponRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -17,21 +18,30 @@ public class ApplyService {
 
     private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    private final AppliedUserRepository appliedUserRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
-        if(count > 100) {
+        if (count > 100) {
             return;
         }
 //       couponRepository.save(new Coupon(userId));
-       couponCreateProducer.create(userId);
+        couponCreateProducer.create(userId);
     }
 
     public CouponRepository getCouponRepository() {
